ab49d321c933780073565a0ed17f1df16f9365f5
arpitasingh2025-art <arpita.singh2025@glbajajgroup.org>
2026-06-10 17:10:04 +0530

    Day-2 Assignment Is Uploaded

---
+++ Day-2/Q3.c
@@ -0,0 +1,17 @@
+// Q3
+#include <stdio.h>
+
+int main() {
+    int r,s=0,n,pro=1;
+    printf("enter a number");
+    scanf("%d",&n);
+    while(n>0)
+    {
+        r=n%10;
+        s=s*10+r;
+        n=n/10;
+        pro=pro*r;
+    }
+    printf("product of number%d",pro);
+    return 0;
+}
