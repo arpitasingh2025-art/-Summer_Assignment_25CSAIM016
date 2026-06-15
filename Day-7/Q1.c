b5c60f34f2e074197d3583e6be2f9e941d66f594
arpitasingh2025-art <arpita.singh2025@glbajajgroup.org>
2026-06-15 23:22:07 +0530

    Day-7 Assignment Is Uploaded.

---
+++ Day-7/Q1.c
@@ -0,0 +1,20 @@
+//Q1
+#include <stdio.h>
+
+int fact(int n);
+int main()
+{
+    int n;
+    printf("Enter a number");
+    scanf("%d",&n);
+    printf("%d", fact(n));
+    return 0;
+}
+int fact(int n)
+{
+    if(n==0)
+        return 1;
+    else
+        return n*fact(n-1);
+         
+}
---
+++ Day-7/Q4.c
@@ -0,0 +1,20 @@
+// Q4
+#include <stdio.h>
+int reverse(int n, int rev);
+int main()
+{
+    int n;
+    printf("Enter a number");
+    scanf("%d",&n);
+
+    printf("Reversed number%d", reverse(n, 0));
+
+    return 0;
+}
+int reverse(int n, int rev)
+{
+    if (n==0)
+        return rev;
+    else
+        return reverse(n/10, rev* 10 + (n % 10));
+}
