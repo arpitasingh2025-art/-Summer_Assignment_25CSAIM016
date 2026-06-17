645dcfeb39471f0a5e05a49816d74d85b790f672
arpitasingh2025-art <arpita.singh2025@glbajajgroup.org>
2026-06-17 16:36:55 +0530

    Day-9 Assignment Is Uploaded

---
+++ Day-9/Q2.c
@@ -0,0 +1,15 @@
+//Q2
+#include<stdio.h>
+int main()
+{
+    int i,j,k=1;
+    for(i=5;i>=1;i--)
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf("%d",j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
