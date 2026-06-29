244a7702478ee0989101b73cfc8fa015173ba9ce
arpitasingh2025-art <arpita.singh2025@glbajajgroup.org>
2026-06-29 14:53:23 +0530

    Day-21 Assignment Is Uploaded

---
+++ Day-21/Q1.c
@@ -0,0 +1,16 @@
+//Q1
+#include <stdio.h>
+
+int main()
+{
+    char s[100];
+    int i=0;
+    printf("Enter a string");
+    scanf("%s",s);
+    while (s[i]!='\0')
+    {
+        i++;
+    }
+    printf("Length of the string %d",i);
+    return 0;
+}
